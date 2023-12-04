2d1c80ae207b8ba6fef8d442402f6f75b4ab6b2f
Francisco Braço-Forte <f.s.f.b2001@gmail.com>
2023-12-04 12:56:27 +0000

    little change in day01

--- day01/src/main.rs
+++ day01/src/main.rs
@@ -81,42 +81,21 @@ fn main() -> Result<(), Box<dyn Error>> {
             // check digit name
             let i = len - i;
             if i < 3 { continue; }
-            r = match line.get(i-3..i) {
-                Some(name) => {
-                    match name {
-                        "one" => 1,
-                        "two" => 2,
-                        "six" => 6,
+            r = match line.get(i-3..i).unwrap() {
+                "one" => 1, "two" => 2, "six" => 6,
+                _ => {
+                    if i < 4 { continue; }
+                    match line.get(i-4..i).unwrap() {
+                        "four" => 4, "five" => 5, "nine" => 9,
                         _ => {
-                            if i < 4 { continue; }
-                            match line.get(i-4..i) {
-                                Some(name) => {
-                                    match name {
-                                        "four" => 4,
-                                        "five" => 5,
-                                        "nine" => 9,
-                                        _ => {
-                                            if i < 5 { continue; }
-                                            match line.get(i-5..i) {
-                                                Some(name) => {
-                                                    match name {
-                                                        "three" => 3,
-                                                        "seven" => 7,
-                                                        "eight" => 8,
-                                                        _ => 0,
-                                                    }
-                                                },
-                                                None => 0,
-                                            }
-                                        },
-                                    }
-                                },
-                                None => 0,
+                            if i < 5 { continue; }
+                            match line.get(i-5..i).unwrap() {
+                                "three" => 3, "seven" => 7, "eight" => 8,
+                                _ => 0,
                             }
                         },
                     }
-                }
-                None => 0,
+                },
             };
             if r != 0 { break; }
         }
